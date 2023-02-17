5fdb81dbbb0fe501dd50034704a7dd8b32506ecf
Gary McQueen <garybracket@hotmail.com>
2023-02-17 14:38:07 -0500

    fixed?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../stylesheets/application.scss"
-import "font-awesome"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
